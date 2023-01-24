9a613a7f612e62bbf82fa46c6ca630599903d424
Akvirac <juanarielvivian@gmail.com>
2023-01-23 22:33:40 -0300

    fixes

--- biblioteca/src/App.vue
+++ biblioteca/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-//Todos mis import
-import ButtonLive from './components/ButtonLive.vue'
-import CreateBook from './components/CreateBook'
-import BookList from './components/BookList.vue'
 import { ref } from 'vue'
+import ButtonLive from './components/ButtonLive.vue'
+import BookList from './components/BookList.vue'
+import CreateBook from './components/CreateBook.vue'
+
 //Variables, arrays, objetos.
 const isFormActive = ref(false)
 const books = ref([
@@ -43,23 +43,21 @@ function buyBook(book) {
 </script>
 
 <template>
-	<div class="grid h-screen place-items-center">
+	<div class="mt-5 grid h-screen place-items-center">
 		<ButtonLive
 			class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-			@click="isFormActive = !isFormActive"
+			@click="isFormActive = true"
 			>Submit a new Book</ButtonLive
 		>
+
 		<ButtonLive
 			class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-			>Buy Book</ButtonLive
-		>
-		<ButtonLive
-			class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+			@click="isFormActive = false"
 			>Book List</ButtonLive
 		>
-		{{ books }}
+		<!-- {{ books }} -->
 
 		<CreateBook v-if="isFormActive" @submit="submitBook" />
-		<BookList v-else />
+		<BookList v-else :books="books" @buy="buyBook" />
 	</div>
 </template>
