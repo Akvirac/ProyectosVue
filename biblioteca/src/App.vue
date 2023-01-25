69d32c6c25bff0b7de387cc33bcfa9d5339caa08
Akvirac <juanarielvivian@gmail.com>
2023-01-25 08:15:41 -0300

    cambiosdesdecasa

--- biblioteca/src/App.vue
+++ biblioteca/src/App.vue
@@ -46,13 +46,13 @@ function buyBook(book) {
 	<div class="mt-5 h-screen place-items-center">
 		<div class="flex justify-center">
 			<ButtonLive
-				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
 				@click="isFormActive = true"
 				>Submit a new Book</ButtonLive
 			>
 
 			<ButtonLive
-				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+				class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
 				@click="isFormActive = false"
 				>Book List</ButtonLive
 			>
